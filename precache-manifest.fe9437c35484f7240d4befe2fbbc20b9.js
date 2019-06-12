13cd26bb73b06ce2b851f70d653cbea56d6239ba
Acid <clockstopfountain@gmail.com>
2019-06-12 17:49:31 +0530

    Updates

---
+++ precache-manifest.fe9437c35484f7240d4befe2fbbc20b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7d42c586a4cb0aa4e21ff13a4a566b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "a1ed251a11a4eeef7a6c",
+    "url": "/my-app/static/css/main.abe6a505.chunk.css"
+  },
+  {
+    "revision": "0ab661f54efff019eca6",
+    "url": "/my-app/static/js/2.c1075f4e.chunk.js"
+  },
+  {
+    "revision": "a1ed251a11a4eeef7a6c",
+    "url": "/my-app/static/js/main.c0b36c89.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
